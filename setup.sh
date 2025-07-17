a4c4d2a90d37017dcb42743dfaa85328279565f4
tsubasaogawa <hurikake@gmail.com>
2025-07-17 15:58:17 +0900

    fix: use dotfiles.d directory

--- setup.sh
+++ setup.sh
@@ -1,40 +1,62 @@
 #!/bin/bash
 
-# Provisioning script for dotfiles
-# No args required
+# .dotfiles.d にあるドットファイルをユーザーのホームディレクトリにシンボリックリンクするスクリプト
+#
+# シンボリック先とシンボリック元 (対象となるドットファイル)
+#  - `.dotfiles.d/.*rc` -> `~/.*rc`
+#  - `.dotfiles.d/.gitconfig` -> `~/.gitconfig`
+#  - `.dotfiles.d/.<DOTFILE>.d/main.*sh` -> `~/.<DOTFILE>`
+#
+# シンボリック元ファイルが既に存在する場合、バックアップを作成する
+# ただし、元ファイルもシンボリックリンクだった場合はバックアップは作成せず上書きする
 
-function showmsg() {
-  echo "$1" 1>&2
+set -eu
+
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)/dotfiles.d"
+HOME_DIR="$HOME"
+
+# シンボリックリンクを作成する関数
+create_symlink() {
+  local src=$1
+  local dest=$2
+
+  if [ -L "$dest" ]; then
+    # 既存のシンボリックリンクは上書き
+    ln -snf "$src" "$dest"
+    echo "Updated symlink: $dest -> $src"
+  elif [ -e "$dest" ]; then
+    # 既存のファイルはバックアップを作成
+    mv "$dest" "${dest}.bak"
+    echo "Backed up existing file: $dest -> ${dest}.bak"
+    ln -sn "$src" "$dest"
+    echo "Created symlink: $dest -> $src"
+  else
+    # 新規作成
+    ln -sn "$src" "$dest"
+    echo "Created symlink: $dest -> $src"
+  fi
 }
 
-showmsg '# create symbolic links'
-readonly core_dir="$(cd $(dirname $0) && pwd)/core"
-for core_rcfile in $(find "$core_dir" -name '.*' -type f); do
-  # get basename
-  rcfilename=$(basename "$core_rcfile")
-  destination_rcfile=~/$rcfilename
-
-  # if symbolic link exists
-  if [[ -e "$destination_rcfile" ]]; then
-    read -p "$destination_rcfile exists. replace? (Y/n)" ans
-    [[ "x$ans" != 'xY' ]] && continue
-    # create backup
-    mv $destination_rcfile{,.$(date +%Y%m%d)}
-    showmsg "created backup ${destination_rcfile}.$(date +%Y%m%d)"
-  # if broken symbolic link exists
-  elif ls "$rcfilename" >/dev/null 2>&1 && find ~/ -maxdepth 1 -name "$rcfilename" -xtype l >/dev/null 2>&1; then
-    rm -f $destination_rcfile
-    showmsg "deleted broken symbolic link $destination_rcfile"
+# .dotfiles.d/.*rc と .gitconfig を処理
+for file in "$DOTFILES_DIR"/.*rc "$DOTFILES_DIR"/.gitconfig; do
+  if [ -f "$file" ]; then
+    filename=$(basename "$file")
+    create_symlink "$file" "$HOME_DIR/$filename"
   fi
-
-  ln -s "$core_rcfile" "$destination_rcfile"
-  [[ $? -eq 0 ]] && showmsg "created symbolic link $rcfilename to your home directory"
 done
 
-showmsg '# install NeoBundle'
-curl --silent https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > /tmp/install.sh
-sh /tmp/install.sh | tee "/tmp/NeoBundle.log.$(date +%Y%m%d%H%M)" | grep 'Complete setup NeoBundle'
-[[ $? -ne 0 ]] && showmsg "error occured. please refer to /tmp/NeoBundle.log.$(date +%Y%m%d%H%M)" && exit 1
+# .dotfiles.d/.*.d/main.*sh を処理
+for dir in "$DOTFILES_DIR"/.*.d; do
+  if [ -d "$dir" ]; then
+    for main_file in "$dir"/main.*; do
+      if [ -f "$main_file" ]; then
+        dir_basename=$(basename "$dir")
+        dest_filename=".${dir_basename%.d}"
+        create_symlink "$main_file" "$HOME_DIR/$dest_filename"
+        break # 各.dディレクトリにつき1つのmainファイルのみ処理
+      fi
+    done
+  fi
+done
 
-showmsg 'setup done.'
-exit 0
+echo "Dotfiles setup complete."
