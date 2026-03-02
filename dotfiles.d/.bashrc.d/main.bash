9b940d88714ed8fa8f26c68a46ee5b4072a53d22
Tsubasa Ogawa <iam@tsubasaogawa.me>
2026-03-02 14:17:49 +0900

    chore: organize aliases

--- dotfiles.d/.bashrc.d/main.bash
+++ dotfiles.d/.bashrc.d/main.bash
@@ -46,12 +46,10 @@ alias rsyncp='rsync -C --filter=":- .gitignore" -acv'
 
 source ~/.local/share/bash-abbrev-alias/abbrev-alias.plugin.bash
 abbrev-alias -c asu='aws-sso-util login --force-refresh'
-abbrev-alias -c gps='git push origin @'
-abbrev-alias -c gpl='git pull'
 abbrev-alias -ge B='$(git symbolic-ref --short HEAD 2>/dev/null)'
-abbrev-alias -c ti='terraform init'
 abbrev-alias -c awscopilot='/usr/local/bin/copilot'
 abbrev-alias -c copilot='copilot --banner'
+abbrev-alias -c cdp='cd $(ls | peco)'
 
 # stop screen lock & enable i-search
 stty stop undef
