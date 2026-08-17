4dcb8cb073d93ce88fc10c532158e02cec701f86
Tsubasa Ogawa <iam@tsubasaogawa.me>
2026-08-17 16:58:48 +0900

    chore: delete zoxide

--- dotfiles.d/.bashrc.d/agents.bash
+++ dotfiles.d/.bashrc.d/agents.bash
@@ -6,9 +6,4 @@ if [ -n "$CLAUDECODE" ]; then
   alias find='fd'
   alias sed='sd'
   # alias ls='eza'
-
-  # cd は組み込みコマンドのため alias ではなく zoxide の cd 統合を使う
-  if command -v zoxide >/dev/null 2>&1; then
-    eval "$(zoxide init bash --cmd cd)"
-  fi
 fi
