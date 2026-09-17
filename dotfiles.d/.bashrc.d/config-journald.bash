511aa389d2cca5568d635573bcae99ee1a910a07
Tsubasa Ogawa <iam@tsubasaogawa.me>
2026-09-17 15:43:40 +0900

    chore(config): add Podman and translate comments to English (#39)
    Add Podman to mise-managed tools and translate shell and tig configuration comments into English.

--- dotfiles.d/.bashrc.d/config-journald.bash
+++ dotfiles.d/.bashrc.d/config-journald.bash
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -eu
 
-# journald.conf の MaxLevelKMsg を notice に設定し、INFO レベルのカーネルメッセージを journald に記録させない
+# Set MaxLevelKMsg to notice so journald ignores INFO-level kernel messages
 
 if [[ ! -f /etc/systemd/journald.conf.d/suppress-kmsg-info.conf ]]; then
   sudo mkdir -p /etc/systemd/journald.conf.d
   printf "[Journal]\nMaxLevelKMsg=notice\n" | sudo tee /etc/systemd/journald.conf.d/suppress-kmsg-info.conf >/dev/null
 fi
-
